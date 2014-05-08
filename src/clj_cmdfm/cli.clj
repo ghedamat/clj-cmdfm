58b3e8a8b789d58db78094720706863574b842e4
Mattia Gheda <ghedamat@gmail.com>
2014-05-08 02:06:42 -0400

    refactoring attempt with a single channel

--- src/clj_cmdfm/cli.clj
+++ src/clj_cmdfm/cli.clj
@@ -25,43 +25,14 @@
   (str "The following errors occurred while parsing your command:\n\n"
        (string/join \newline errors)))
 
-(defn exit [status msg]
-  (println msg)
-  (System/exit status))
-
-(defn play-genre
-  [genre]
-  (let [files (atom (->>
-                      (api/fetch-genre genre 10)
-                      (map #(select-keys % [:title :stream_url :duration]))))
-        mplayer-chan (chan)
-        command-chan (chan)]
-    (main-loop files mplayer-chan command-chan)
-    (play-next files mplayer-chan)
-    (println "choose an action: [quit next pause]")
-    (loop [input (read-line)]
-      (do
-        (>!! command-chan input)
-        (recur (read-line))))))
-
 (defn -main [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
     ;; Handle help and error conditions
     (cond
       (:help options) (exit 0 (usage summary))
-      (< (count arguments) 1) (exit 1 (usage summary))
+      (> (count arguments) 1) (exit 1 (usage summary))
       errors (exit 1 (error-msg errors)))
     ;; Execute program with options
-    (if (seq arguments)
-      (play-genre (first arguments))
-      (exit 1 (usage summary)))))
+    (command-loop arguments)))
 
 
-(comment
-  (def mplayer-chan (chan))
-  (def command-chan (chan))
-
-  (def files (atom (->>
-    (api/fetch-genre :techno 10)
-    (map #(select-keys % [:title :stream_url :duration])))))
-)
--- src/clj_cmdfm/core.clj
+++ src/clj_cmdfm/core.clj
@@ -1,43 +1,102 @@
 (ns clj-cmdfm.core
-  (:require [clojure.core.async :refer [>!! alt! chan >! <! go <!! alt!!]]
+  (:require [clojure.core.async :refer [>!! alt! chan >! <! go go-loop <!! alt!! put!]]
             [clj-cmdfm.mplayer :as mplayer]
-            [clj-cmdfm.api :as api])
+            [clj-cmdfm.api :as api]
+            [clojure.string :as string])
   (:gen-class))
 
+(defn truef [& o] true)
+(defn falsef [& o] false)
 
+(defn play-track
+  [files app]
+  (let [c (:channel app)
+        file (first files)]
+    (if (seq files)
+      (go
+        (println "playing" (:title file))
+        (mplayer/play (:stream_url file))
+        (println "finished playing" (:title file))
+        (>! c [:play-done (rest files)]))
+      (do (swap! (:playing app) falsef)
+          (println "playlist is over")))))
 
-(defn delayed-play
-  [file c]
-  (go
-    (println "playing" (:title file))
-    (mplayer/play (:stream_url file))
-    (println "finished playing" (:title file))
-    (>! c :done)))
+(defn play-genre
+  [genre app]
+  (let [c (:channel app)
+        playing (:playing app)
+        files (->> (api/fetch-genre genre 10)
+                   (map #(select-keys % [:title :stream_url :duration])))]
+    (if @playing
+      (println "already playing")
+      (do (swap! (:playing app) truef)
+          (play-track files app)))))
 
 (defn play-next
-  [files c]
-  (if (seq @files)
-    (do (delayed-play (first @files) c)
-        (swap! files rest))))
+  [app]
+  (if @(:playing app)
+    (mplayer/stop)
+    (println "nothing to play")))
 
-(defn exec-command
-  [cmd files mplayer-chan]
-  (case cmd
-    "pause" (mplayer/pause)
-    "next" (do (mplayer/stop) (<!! mplayer-chan) (play-next files mplayer-chan))
-    "quit" (do (mplayer/stop) (<!! mplayer-chan) nil)
-    (do (println "valid operations are [pause next quit]") :no-op)))
+(defn play-done
+  [params app]
+  (if @(:playing app)
+    (play-track params app)))
+
+(defn stop
+  [app]
+  (swap! (:playing app) falsef)
+  (mplayer/stop))
+
+(defn pause
+  []
+  (mplayer/pause))
+
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn help []
+  (println "Available commands:
+           play <genere>
+           next
+           pause
+           stop
+           quit"))
+
+(defn dispatch
+  [command params app]
+  (case command
+    :play (play-genre params app)
+    :play-done (play-done params app)
+    :next (play-next app)
+    :pause (pause)
+    :stop (stop app)
+    :quit (exit 0 "Bai Bai")
+    :test (println "test")
+    :save (println "save")
+    (help)))
+
+(def app
+  {:playing (atom false)
+   :channel (chan)})
 
 (defn main-loop
-  [files mplayer-chan command-chan]
-  (go
-    (loop [n :start]
-      (if (nil? n)
-        (do
-          (println "playlist is over")
-          (System/exit 0))
-        (recur
-          (alt!
-            mplayer-chan (play-next files mplayer-chan)
-            command-chan ([cmd] (exec-command cmd files mplayer-chan))))))))
+  []
+  (let [channel (:channel app)]
+    (go-loop []
+             (let [[command params] (<! channel)]
+               (dispatch command params app)
+               (recur)))))
+
+(defn command-loop
+  [_]
+  (println "Welcome to clj-cmdfm")
+  (help)
+  (main-loop)
+  (loop []
+    (let [[command & params] (-> (read-line) (string/split #"\s+"))]
+      (dispatch (keyword command) params app)
+      (recur))))
 
