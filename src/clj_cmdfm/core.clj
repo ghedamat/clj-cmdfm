5f5a6eae93995c0433012f3486c6512e2debfba0
Mattia Gheda <ghedamat@gmail.com>
2014-05-15 21:05:17 -0400

    refactor options mgmt, inspiriation from clojure.tools.cli

--- src/clj_cmdfm/core.clj
+++ src/clj_cmdfm/core.clj
@@ -8,6 +8,9 @@
 (defn truef [& o] true)
 (defn falsef [& o] false)
 
+(defn prn-help [options]
+  (println (str "Available commands:\n\n"
+                (string/join "\n" (map #(:desc %) options)))))
 (def app
   {:playing (atom false)
    :channel (chan)})
@@ -59,14 +62,6 @@
   (println msg)
   (System/exit status))
 
-(defn prn-help []
-  (println "Available commands:
-           play <genre>
-           next
-           pause
-           stop
-           quit"))
-
 (defn dispatch
   [command params app]
   (case command
@@ -75,8 +70,54 @@
     :next (play-next app)
     :pause (pause)
     :stop (stop app)
-    :quit (exit 0 "Bai Bai")
-    (prn-help)))
+    :quit (exit 0 "Bai Bai")))
+
+
+(defn compile-opts
+  [spec]
+  (let [sopt-lopt-desc (take-while #(or  (string? %)  (nil? %)) spec)
+        spec-map  (apply hash-map  (drop  (count sopt-lopt-desc) spec))
+        [short-opt long-opt desc] sopt-lopt-desc
+        validate (:validate spec-map)
+        [validate-fn validate-msg] (when (seq validate)
+                                     (->> (partition 2 2 (repeat nil) validate)
+                                          (apply map vector)))]
+    {:short-opt short-opt
+     :long-opt long-opt
+     :desc desc
+     :validate-fn validate-fn
+     :validate-msg validate-msg}))
+
+(defn- validate [spec values]
+  (let [{:keys [validate-fn validate-msg]} spec]
+    (or (loop [[vfn & vfns] validate-fn [msg & msgs] validate-msg]
+          (when vfn
+            (if (try (apply vfn values) (catch Throwable e))
+              (recur vfns msgs)
+              [::error msg])))
+        [spec values])))
+
+
+(defn dispatch-console
+  [command params app options]
+  (let [option (-> (filter (fn [o]
+                             (if (or (= (:short-opt o) command)
+                                     (= (:long-opt o) command))
+                               o))
+                           options)
+                   first)
+        [status value] (validate option params)]
+    (if (nil? status)
+      (do
+        (println "no such command")
+        (prn-help options))
+      (if (= status ::error)
+        (println "error " command value)
+        (dispatch (keyword (:long-opt option)) params app)))))
+
+(comment
+(dispatch-console "p" nil app opts)
+  )
 
 (defn main-loop
   "Go routine that loops waiting for commands on the app channel."
@@ -87,15 +128,30 @@
                (dispatch command params app)
                (recur)))))
 
+(def cmd-opts
+  [;; First three strings describe a short-option, long-option with optional
+   ;; example argument description, and a description. All three are optional
+   ;; and positional.
+   ["p" "play" "play <genre> [limit]" :validate  [#(> (count %&) 0)  "Must have a genre"]]
+   ["s" "stop" "stop playing"]
+   ["w" "quit" "quit"]
+   ["a" "pause" "pause"]
+   ["n" "next" "next song"]])
+
+(def opts (map compile-opts cmd-opts))
+
+
+
+
 (defn command-loop
   "Reads from stdin for the next command and dispatches it."
   ([]
    (println "Welcome to clj-cmdfm")
-   (prn-help)
+   (prn-help opts)
    (main-loop)
    (loop []
      (let [[command & params] (-> (read-line) (string/split #"\s+"))]
-       (dispatch (keyword command) params app)
+       (dispatch-console command params app opts)
        (recur))))
   ([genre]
    (dispatch :play genre app)
